9b268c8ecb737744539bc2bc4f33d031ecbef773
Victor Tsvetkov <viktor.tsvetkov@raiffeisen.ru>
2016-08-10 17:21:37 +0300

    day10

--- src/main/scala/Day10/Solution.scala
+++ src/main/scala/Day10/Solution.scala
@@ -10,6 +10,10 @@ object Solution {
     val sc = new java.util.Scanner (System.in)
     val n = sc.nextInt()
 
+    def calc(n:Int):Int = {
+      calc_(f(n), 0, 0)
+    }
+
     def f(n:Int):List[Int] = {
       val arr:List[Int] = Nil
       f_(n, arr)
@@ -17,12 +21,16 @@ object Solution {
 
     def f_(n:Int, arr:List[Int]):List[Int] = n match {
       case 0 => arr
-      case _ => f_(n/2, arr.::(n%2))
+      case _ => f_(n/2, n%2 :: arr)
     }
 
-    println(f(n) mkString "")
-    // todo
+    def calc_(arr:List[Int], globMax:Int, currMax:Int):Int = arr match {
+      case Nil => if (globMax == 0) currMax else globMax
+      case 1 :: tail => calc_(arr.tail, globMax, currMax+1)
+      case 0 :: tail => calc_(arr.tail, if (globMax > currMax) globMax else currMax, 0)
+    }
 
+    println(calc(n))
   }
 }
 
