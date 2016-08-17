78cfeac486096a6b49cb836d5fc4627075c071b2
Victor Tsvetkov <viktor.tsvetkov@raiffeisen.ru>
2016-08-17 18:29:54 +0300

    day11

--- src/main/scala/Day11/Solution.scala
+++ src/main/scala/Day11/Solution.scala
@@ -1,7 +1,4 @@
 package main.scala.Day11
-
-import scala.collection.mutable.ListBuffer
-
 /**
   * Created by user on 8/12/16.
   */
@@ -9,30 +6,12 @@ object Solution {
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner (System.in)
-    var arr = Array.ofDim[Int](6,6)
+    val arr = Array.ofDim[Int](6,6)
     for(arr_i <- 0 to 6-1) {
       for(arr_j <- 0 to 6-1) {
         arr(arr_i)(arr_j) = sc.nextInt()
       }
     }
-
-
-
-/*
-    def maximumCalculatedHourglassesSum(arr:Array[Int][Int]):Int = {
-//      CalculatedHourglassesSum(arr:Array[Int][Int]):Int =
-      ???
-    }
-
-    def CalculatedHourglassesSum(arr:Array[Int][Int], globMax:Int, currMax:Int):Int = {
-//      HourglassesSum(arr:Array[Int][Int], globMax:Int, currMax:Int):Int =
-      ???
-    }
-
-    def HourglassesSum(arr:Array[Int][Int], globMax:Int, currMax:Int):Int = {
-      ???
-    }
-*/
 
     def smallMass(arr:Array[Array[Int]], x:Int, y:Int) = {
       for {
@@ -41,6 +20,11 @@ object Solution {
       } yield arr(i)(j)
     }
 
-    println(smallMass(arr,0,0).foreach(println))
+    val list = smallMass(arr, 0, 0).toList.zipWithIndex.filter(_._2 != 5).filter(_._2 != 3).map(_._1)
+
+
+//    println(smallMass(arr,0,0).foreach(println))
+//    println(smallMass(arr,0,0))
+    println(list)
   }
 }
